496c1cd55b9be5c4967a87bfe8986ebf7a6a380a
ArdianLegansyah73 <121847053+ArdianLegansyah73@users.noreply.github.com>
2026-04-27 11:48:38 +0800

    Update memasukan angka positif.py

--- memasukan angka positif.py
+++ memasukan angka positif.py
@@ -1,14 +1,14 @@
-while True:
-    try:
+while True: #perulangan tanpa batas true artinya benar,jadi program akan jalan
+    try:    #untuk menangani error
         # Meminta input dari user
-        angka = float(input("Masukkan angka: "))
+        angka = float(input("Masukkan angka: ")) #float untuk mengubah input angka desimal
         
         # Mengecek apakah angka positif
-        if angka > 0:
+        if angka > 0: #mengecek apakah angka lebih dari 0 positif
             print(f"Terima kasih! Anda memasukkan angka positif: {20}")
             break # Keluar dari loop jika positif
-        else:
-            print("Harus positif!")
+        else: # kondisi if tidak terpenuhi
+            print("Harus positif!") #output kalau angka nol atau negatif program mengulang lagi le atas loop
             
     except ValueError:
         print("Input tidak valid! Masukkan angka.")
