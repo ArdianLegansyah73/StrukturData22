8bb642040130698e510029ad090711cbdaced5c9
ArdianLegansyah73 <121847053+ArdianLegansyah73@users.noreply.github.com>
2026-04-27 11:38:57 +0800

    Update program hitung umur.py
    Added comments to clarify the conditions and input handling.

--- program hitung umur.py
+++ program hitung umur.py
@@ -1,14 +1,14 @@
 # Program penghitung umur
 
-umur = int(input("Masukkan umur anda: "))
+umur = int(input("Masukkan umur anda: "))#untuk menerima input dari user
 
-if umur >= 22 :
+if umur >= 22 :#jika lebih dari atau sama dengan 22 maka jalankan kode di bawah
     print("anda cukup umur")
-elif umur < 18:
+elif umur < 18: #di cek kalau kondisi sebelumnya tidak terpenuhi jika umur kurang dari 18
     print("anda belum cukup umur")
-elif umur < 0:
+elif umur < 0: #komdisi ini tidak akan pernah jlan,karena umur 18 sudah menangkap semua angka negatif
     print("anda belum lahir")
-elif umur > 60:
+elif umur > 60: #karena sebelumnya umur 22 maka umur 60 sudah masuk kondisi pertama jika kondisi ini tidak pernah dijalankan
     print("banyakin ibadah, bentar lagi mati")
 
 print("Program selesai")
